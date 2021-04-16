1df263024560dbc453e5a9af38b74154d7867ccd
aryan02420 <aryan02420@gmail.com>
2021-04-17 02:24:08 +0530

    set valve status

--- vlvCls.asm
+++ vlvCls.asm
@@ -1,9 +1,10 @@
 vlvCls proc near
-	cmp	vlvsts, ffh
+	cmp	vlvSts, ffh
 	jne	vc_x0
 	mov	al, 00000011b
 	out	portC2, al
 	mov	al, 00000010b
 	out	portC2, al
+	mov	vlvSts, 00h
 vc_x0:	ret
 vlvCls endp
--- vlvOpn.asm
+++ vlvOpn.asm
@@ -5,5 +5,6 @@ vlvOpn proc near
 	out	portC2, al
 	mov	al, 00000010b
 	out	portC2, al
+	mov	vlvSts, 0ffh
 vo_x0:	ret
 vlvOpn endp
