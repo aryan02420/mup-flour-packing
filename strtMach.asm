0dd89ae468d5f7f452a58a033613e597f4ee9f84
aryan02420 <aryan02420@gmail.com>
2021-04-17 17:35:11 +0530

    added designs

--- strtMach.asm
+++ strtMach.asm
@@ -1,12 +1,13 @@
 strtMach proc near
-	call	getATemp
+	call	calcDelay
+sm_loop:call	getATemp
 	mov	bl, cUsrTemp
 	add	bl, 5
 	cmp	cActTemp,  bl
 	jbe	sm_norm
 	call	ringAlm
 	jmp	sm_ret
-sm_norm:call	calcDelay
-
+sm_norm:
+	jmp	sm_loop
 sm_ret:	ret
 strtMach endp
